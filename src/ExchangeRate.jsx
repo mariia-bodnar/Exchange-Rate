5a476d42083a892e1412a4434265783925e82113
Mariia Bodnar <mbodnar548@gmail.com>
2022-10-13 19:36:19 +0300

    Setup currencies fetch

--- src/ExchangeRate.jsx
+++ src/ExchangeRate.jsx
@@ -1,14 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { axiosInstance } from "./axios";
 
 export const ExchangeRate = () => {
+  const [currencies, setCurrencies] = useState([]);
+
   useEffect(() => {
     axiosInstance.get("/currency_data/list").then((res) => {
-      console.log(res.json);
+      const currencyList = Object.entries(res.data.currencies).map(
+        ([key, value]) => ({ id: key, label: value })
+      );
+      setCurrencies(currencyList);
     });
-  });
-  return <S.Box></S.Box>;
+  }, []);
+  return (
+    <S.Box>
+      <div>
+        <select multiple={false}>
+          {currencies.map(c => (<option key={c.id} >{c.label}<strong>({c.id})</strong></option>))}
+          {/* <option value={1}>First option</option> */}
+        </select>
+      </div>
+    </S.Box>
+  );
 };
 const S = {
   Box: styled.div`
